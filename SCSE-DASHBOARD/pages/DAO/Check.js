bd5982cd1aabf9fb6fdf2928a913b591001a0242
Le Trong Hieu <eotihvn@gmail.com>
2021-09-21 11:52:18 +0700

    format lại code

--- SCSE-DASHBOARD/pages/DAO/Check.js
+++ SCSE-DASHBOARD/pages/DAO/Check.js
@@ -1,25 +1,21 @@
 var jwtdata = localStorage.getItem("token");
-if(jwtdata == null)
-{
-    window.location.href="../samples/login.html"
+if (jwtdata == null) {
+    window.location.href = "../samples/login.html"
 }
-else 
-{
+else {
     var role = parseJwt(localStorage.getItem("token"));
-    if(role.nameid[1]=== "Admin")
-    {
+    if (role.nameid[1] === "Admin") {
         loadData()
     }
-    else
-    {
+    else {
         alert('Cútttttttttttttttt')
-        window.location.href="../QuanLy/DangBai/Index.html"
+        window.location.href = "../QuanLy/DangBai/Index.html"
     }
 }
 function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
--- SCSE-DASHBOARD/pages/DAO/Login_Check.js
+++ SCSE-DASHBOARD/pages/DAO/Login_Check.js
@@ -2,7 +2,7 @@ var $getToken = localStorage.getItem("token");
 // Hàm này còn phải check dữ liệu coi có đúng người không!
 // Để tránh trường hợp chỉ cần add new token là xong
 // Trong lúc check xem kiểm tra token đưa vào có đúng chuẩn hay không !
-if(!$getToken){
+if (!$getToken) {
     alert("Vui lòng đăng nhập")
-    window.location.href="./login.html"
+    window.location.href = "./login.html"
 }
