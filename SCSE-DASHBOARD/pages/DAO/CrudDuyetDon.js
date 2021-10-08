afcd4abe95ad7ecb500a67b6d94977e5a1ddbfab
DESKTOP-JL5NQM2\baoda <dqbtt2000@gmail.com>
2021-10-08 15:35:31 +0700

    a

--- SCSE-DASHBOARD/pages/DAO/CrudDuyetDon.js
+++ SCSE-DASHBOARD/pages/DAO/CrudDuyetDon.js
@@ -1,27 +1,40 @@
-const WEB_API = "http://localhost:59360/";
-window.addEventListener('load', loadData)
-
-async function loadData() {
-    fetch(WEB_API + "/Management/XemDanhSachDangKy")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            var html = response.map(function (response) {
-                let { ID, FirstName, LastName, DOB, Phone, Email, Address, Project, Purpose, IDState } = response;
-                if(IDState === 1){
-                    IDState = 'Pending'
-                }
-                if(IDState === 2){
-                    IDState = 'Approved'
-                }
-                if(IDState === 3){
-                    IDState = 'NotApproved'
-                }
-                if(IDState === 4){
-                    IDState = 'Deleted'
-                }
-                return `<tr>
+const url = "http://localhost:59360/";
+(function ($) {
+    const fields = [
+        { name: 'ID', title: 'ID' },
+        { name: 'FirstName', title: 'FirstName' },
+        { name: 'LastName', title: 'LastName' },
+        { name: 'DOB', title: 'DOB' },
+        { name: 'Phone', title: 'Phone' },
+        { name: 'Email', title: 'Email' },
+        { name: 'Address', title: 'Address' },
+        { name: 'Project', title: 'Project' },
+        { name: 'Purpose', title: 'Purpose' },
+        { name: 'IDState', title: 'IDState' },
+    ]
+    'use strict';
+    $(function () {
+        fetch(url + "/Management/XemDanhSachDangKy")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                var result = response.filter(v => v.IDState !== 4)
+                var html = result.map(function (response) {
+                    let { ID, FirstName, LastName, DOB, Phone, Email, Address, Project, Purpose, IDState } = response;
+                    if (IDState === 1) {
+                        IDState = 'Pending'
+                    }
+                    if (IDState === 2) {
+                        IDState = 'Approved'
+                    }
+                    if (IDState === 3) {
+                        IDState = 'NotApproved'
+                    }
+                    if (IDState === 4) {
+                        IDState = 'Deleted'
+                    }
+                    return `<tr>
                     <td>${ID}</td>
                     <td>${FirstName}</td>
                     <td>${LastName}</td>
@@ -33,98 +46,29 @@ async function loadData() {
                     <td>${Purpose}</td>
                     <td>${IDState}</td>
                     <td><button onclick="return getData(${ID})" class="btn btn-outline-primary">View</button>
-                    <button onclick="return deleteData(${ID})" class="btn btn-outline-primary">Delete</button><td>
-                    </tr>`
+                    <button onclick="return deleteData(${ID})" class="btn btn-outline-primary">Delete</button></td>
+                    </tr>`;
+                })
+                $('#tbody').html(html);
+                $(document).ready(function () {
+                    $('#dataTable').DataTable({
+                        "order": [[0, "desc"]]
+                    });
+                });
             })
-            $('.tbody').html(html);
-            //Thiếu IDState
-        })
-}
-async function getData(ID) {
-    fetch(WEB_API + "/Management/GetByIdNguoiDangKy?id=" + ID)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            const { ID, FirstName, LastName, DOB, Phone, Email, Address, Project, Purpose , IDState } = response;
-            $('#ID').val(ID);
-            $('#FirstName').val(FirstName);
-            $('#LastName').val(LastName);
-            $('#DOB').val(DOB);
-            $('#Phone').val(Phone);
-            $('#Email').val(Email);
-            $('#Address').val(Address);
-            $('#Project').val(Project);
-            $('#Purpose').val(Purpose);
-            $('#IDState').val(IDState);
-        })
-    $('#exampleModal-2').modal('show');
-    $('#add').hide();
-    $('#edit').show();
-}
-(function($) {
-    'use strict';
-    $(function() {
-      $('#order-listing').DataTable({
-        "aLengthMenu": [
-          [5, 10, 15, -1],
-          [5, 10, 15, "All"]
-        ],
-        "iDisplayLength": 10,
-        "language": {
-          search: ""
-        }
-      });
-      $('#order-listing').each(function() {
-        var datatable = $(this);
-        // SEARCH - Add the placeholder for Search and Turn this into in-line form control
-        var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
-        search_input.attr('placeholder', 'Search');
-        search_input.removeClass('form-control-sm');
-        // LENGTH - Inline-Form control
-        var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
-        length_sel.removeClass('form-control-sm');
-      });
+            .catch(error => {
+                throw error;
+            })
     });
-  })(jQuery);
-async function addData() {
-    var $data = {
-        FirstName: $('#FirstName').val(),
-        LastName: $('#LastName').val(),
-        DOB: $('#DOB').val(),
-        Phone: $('#Phone').val(),
-        Email: $('#Email').val(),
-        Address: $('#Address').val(),
-        Project: $('#Project').val(),
-        Purpose: $('#Purpose').val(),
-        IDState: $('#IDState').val(),
-    };
-    fetch(WEB_API + "Management/DangKiThamGia", {
-        method: 'POST',
-        body: JSON.stringify($data),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-        },
-    }).then(function (response) {
-        return response.json()
-    })
-        .then(function (data) {
-            if (data.Status === 'Success') {
-                alert('Thêm Thành Công')
-                window.location.reload();
-            }
-            else {
-                alert('Data not insert')
-            }
-        })
-}
-async function updateData() {
-    var data = {
-        ID: $('#ID').val(),
-        IDState: $('#IDState').val(),
-    };
-    debugger
-    fetch(WEB_API + "Management/DangKiThamGia", {
+})(jQuery);
+
+
+function deleteTamThoi(ID) {
+    const data = {
+        IDPost: ID,
+        IDState: 4,
+    }
+    fetch(url + "/User/ThemTaiKhoan", {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -143,8 +87,113 @@ async function updateData() {
             }
         })
 }
-async function deleteData(ID) {
-    fetch(WEB_API + "Management/XoaNguoiDangKy?id=" + ID, {
+async function addData() {
+    var dulieu = {
+        IDCat: $('#IDCat').val(),
+        Title: $('#Title').val(),
+        Slug: $('#Slug').val(),
+        Details: $('#Details').val(),
+        Image: $('#Image').val(),
+        Video: $('#Video').val(),
+        Author: $('#Author').val(),
+        Status: $('#Status').val()
+    };
+    fetch(url + "/Management/ThemBaiViet", {
+        method: 'POST',
+        body: JSON.stringify(dulieu),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.href = "./QuanLyBaiDang.html";
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
+
+async function updateData() {
+    var dulieu = {
+        IDPost: $('$IDPost').val(),
+        IDCat: $('#IDCat').val(),
+        Title: $('#Title').val(),
+        Slug: $('#Slug').val(),
+        Details: $('#Details').val(),
+        Image: $('#Image').val(),
+        Video: $('#Video').val(),
+        Author: $('#Author').val(),
+        Status: $('#Status').val()
+    };
+    fetch(url + "/User/ThemTaiKhoan", {
+        method: 'POST',
+        body: JSON.stringify(dulieu),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Updated') {
+                alert('Sửa Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not update')
+            }
+        })
+}
+function getBaseUrl() {
+    var file = document.querySelector('input[type=file]')['files'][0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        baseString = reader.result;
+        $('#Image').val(baseString);
+        document.getElementById("Image1").src = baseString;
+    };
+    reader.readAsDataURL(file);
+    //console.log(baseString)
+}
+async function autoUpdate(baseString) {
+    var $data = {
+        IDPost: $('£IDPost').val(),
+        IDCat: $('#IDCat').val(),
+        Title: $('#Title').val(),
+        Slug: $('#Slug').val(),
+        Details: $('#Details').val(),
+        Image: baseString,
+        Video: $('#Video').val(),
+        Author: $('#Author').val(),
+        Status: $('#Status').val()
+    }
+    fetch(WEB_API + "Management/ThemBaiViet", {
+        method: 'POST',
+        body: JSON.stringify($data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Updated') {
+                alert('Sửa Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not update')
+            }
+        })
+
+}
+async function deleteData(IDPost) {
+    fetch(url + "/Management/XoaBaiViet?idposts=" + IDPost, {
         method: 'DELETE',
     }).then(function (response) {
         return response.json()
@@ -160,16 +209,16 @@ async function deleteData(ID) {
         })
 }
 function clearTextBox() {
-    $('#ID').val("");
-    $('#FirstName').val("");
-    $('#LastName').val("");
-    $('#DOB').val("");
-    $('#Phone').val("");
-    $('#Email').val("");
-    $('#Address').val("");
-    $('#Project').val("");
-    $('#Purpose').val("");
-    $('#IDState').val("");
+    $('#IDPost').val("");
+    $('#IDCat').val("");
+    $('#Title').val("");
+    $('#Slug').val("");
+    $('#Details').val("");
+    $('#Image').val("");
+    $('#Video').val("");
+    $('#Author').val("");
+    $('#Status').val("");
+
     $('#exampleModal-2').modal('show');
     $('#add').show();
     $('#edit').hide();
