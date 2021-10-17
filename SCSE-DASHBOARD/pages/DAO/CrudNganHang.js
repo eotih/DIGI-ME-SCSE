7fea3f5b61db7cb708ed36d0827c16501ec6a9bd
My Nguyen <myoutybay@gmail.com>
2021-10-18 00:44:24 +0700

    done Post model

--- SCSE-DASHBOARD/pages/DAO/CrudNganHang.js
+++ SCSE-DASHBOARD/pages/DAO/CrudNganHang.js
@@ -12,11 +12,15 @@ const WEB_API = "http://localhost:59360/API/";
                     let { ID, ImageQR, BankName } = response;
                     return `<tr>
                     <td>
+                    <div class="d-flex">
                           <img style="height:100px;width:100px" src='${ImageQR}'/>
                           <h4>${BankName}</h4>
+                    </div>
                     </td>
                     <td>
-                    <button onclick="return getData(${ID})" class="btn btn-outline-danger">Xoá</button></td>
+                    <button onclick="return getData(${ID})" class="btn btn-outline-primary">View</button>
+                    <button onclick="return deleteData(${ID})" class="btn btn-outline-danger">delete</button>
+                    </td>
                     </tr>`;
                 })
                 $('#tbody').html(html);
@@ -31,3 +35,75 @@ const WEB_API = "http://localhost:59360/API/";
             })
     });
 })(jQuery);
+
+async function getData(ID){
+    fetch(WEB_API + "Interface/GetByIdBankInfo?ID=" + ID)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            let { ID, ImageQR, BankName } = response;
+            $('#ID').val(ID);
+            $('#Name').val(BankName);
+            document.getElementById('img').src = ImageQR;
+        })
+    $('#exampleModal-2').modal('show');
+    $('#add').hide();
+    $('#edit').show();
+}
+
+async function addData() {
+    var data = {
+        BankName: $('#Name').val(),
+        ImageQR: $('#img').val(),
+    };
+    console.log(data);
+    fetch(WEB_API + "Interface/AddOrEditBankInfo", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
+async function deleteData(ID){
+    if (confirm('Bạn có muốn xoá ngân hàng?')) {
+
+        fetch(WEB_API + "Interface/DeleteBankInfo?ID=" + ID, {
+            method: "DELETE",
+        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                if (data.Status === 'Delete') {
+                    alert('Xoá thành công')
+                    window.location.reload();
+                }
+                else {
+                    alert('Data not deleted')
+                }
+            })
+    } else {
+
+    }
+}
+function clearTextBox(){
+    $('#ID').val('');
+    $('#Name').val('');
+    $('#img').val('');
+    $('#exampleModal-2').modal('show');
+    $('#add').show();
+    $('#edit').hide();
+}
---
+++ SCSE-DASHBOARD/pages/DAO/CrudPostTinTuc.js
@@ -0,0 +1,81 @@
+const WEB_API = "http://localhost:59360/API/";
+// ------------------------ TIẾNG VIỆT ------------------------ //
+
+async function addPost() {
+    var data = {
+        Title: $('#tieude').val(),
+        Details: $('#summernote').summernote('code'),
+        IDField: $('#theloai').val(),
+        Image: $('#hinhanh').val(),
+        Author: $('#tacgia').val(),
+    };
+    fetch(WEB_API + "Management/AddOrEditNewsVN", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
+// ------------------------ TIẾNG ANH ------------------------ //
+
+window.addEventListener('load', getData)
+function getData() {
+    fetch(WEB_API + "Management/ListNewsNotVersionEN")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            var Category = document.createElement("option");
+            Category.innerHTML = "Vui lòng chọn";
+            Category.value = 0;
+            document.getElementById("PostVN").appendChild(Category);
+            for (var i = 0; i < response.length; i++) {
+                var ele = document.createElement("option");
+                ele.value = response[i].IDPost;
+                ele.innerHTML = response[i].Title;
+                document.getElementById("PostVN").appendChild(ele);
+            }
+        })
+}
+
+
+async function addPostEN() {
+    var data = {
+        IDNewsEN: $('#PostVN').val(),
+        IDField: $('#Category').val(),
+        Title: $('#Title').val(),
+        Details: $('#ENGPOST').summernote('code'),
+        Image: $('#img').val(),
+        Author: $('#Author').val(),
+    };
+    fetch(WEB_API + "Management/AddNewsEN", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
