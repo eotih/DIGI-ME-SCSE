a00e1509dc7997ff963bee83db99cc5c6a42593e
enditidev <enditidev@gmail.com>
2021-11-14 14:40:44 +0700

    done tai lieu tieng anh

--- SCSE-DASHBOARD/pages/DAO/CrudQuanLyTaiLieu.js
+++ SCSE-DASHBOARD/pages/DAO/CrudQuanLyTaiLieu.js
@@ -1,5 +1,6 @@
-const url = "https://api.scse-vietnam.org/API/";
+const url = "http://localhost:59360/API/";
 window.addEventListener('load', loadData)
+window.addEventListener('load', getListDocumentNotVersionEN)
 async function loadData() {
     fetch(url + "Interface/ListDocument")
         .then(function (response) {
@@ -21,6 +22,85 @@ async function loadData() {
             $('#tbody').html(html);
         })
 }
+(function ($) {
+    'use strict';
+    $(function () {
+        fetch(url + "Interface/ListDocument")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                var html = response.map(function (response) {
+                    const { ID, Title, Slug,Details } = response;
+                    return `<tr>
+                    <td>${ID}</td>
+                <td>${Title}</td>
+                <td>${Slug}</td>
+                <td>
+                <button onclick="deleteData(${ID})" class="btn btn-outline-primary">Delete</button></td>
+                </tr>`;
+                })
+                $('#tbody').html(html);
+                $(document).ready(function () {
+                    $('#dataTable').DataTable({
+                        "order": [[0, "asc"]]
+                    });
+                });
+            })
+            .catch(error => {
+                throw error;
+            })
+    });
+})(jQuery);
+// -------------------------------- TIẾNG ANH -------------------------------- //
+(function ($) {
+    'use strict';
+    $(function () {
+        fetch(url + "Interface/ListDocumentEN")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                var html = response.map(function (response) {
+                    const { IDEN, Title, SlugEN,Details } = response;
+                    return `<tr>
+                    <td>${IDEN}</td>
+                <td>${Title}</td>
+                <td>${SlugEN}</td>
+                <td>
+                <button onclick="deleteData(${IDEN})" class="btn btn-outline-primary">Delete</button></td>
+                </tr>`;
+                })
+                $('#tbodyENG').html(html);
+                $(document).ready(function () {
+                    $('#dataTableENG').DataTable({
+                        "order": [[0, "asc"]]
+                    });
+                });
+            })
+            .catch(error => {
+                throw error;
+            })
+    });
+})(jQuery);
+function getListDocumentNotVersionEN() {
+    fetch(url + "Interface/ListDocumentNotVersionEN")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            var Category = document.createElement("option");
+            Category.innerHTML = "Vui lòng chọn";
+            Category.value = 0;
+            document.getElementById("IDEN").appendChild(Category);
+            for (var i = 0; i < response.length; i++) {
+                var ele = document.createElement("option");
+                ele.value = response[i].ID;
+                ele.innerHTML = response[i].Title;
+                document.getElementById("IDEN").appendChild(ele);
+            }
+        })
+}
 async function addData() {
     var data = {
         Title: $('#Title').val(),
@@ -28,6 +108,32 @@ async function addData() {
 
     };
     fetch(url + "Interface/AddOrEditDocument", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
+async function addDataEN() {
+    var data = {
+        IDEN: $('#IDEN').val(),
+        Title: $('#chude').val(),
+        Details: $('#ENGPOST').summernote('code'),
+
+    };
+    fetch(url + "Interface/AddDocumentEN", {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -100,7 +206,6 @@ function getBaseWEB_API() {
         document.getElementById("Image1").src = baseString;
     };
     reader.readAsDataWEB_API(file);
-    //console.log(baseString)
 }
 
 async function deleteData(ID) {
