31bdb5885f720ba463059e11add1193dbf30ec96
My Nguyen <myoutybay@gmail.com>
2021-11-20 16:49:50 +0700

    Crud Trang chinh

--- SCSE-DASHBOARD/pages/DAO/CrudTrangChinh.js
+++ SCSE-DASHBOARD/pages/DAO/CrudTrangChinh.js
@@ -4,31 +4,32 @@ let NewsVN, NewsEN, PostVN, PostEN, Volunteers, PendingAccount = [];
 const getAllData = async (url) => {
     return (await fetch(url)).json();
 }
+const c = "Có "
 const n = " bài"
 const ns =" bài viết hiện đang chờ duyệt"
 async function getNew() {
     CountVI = await getAllData(URL_API + "Management/GetCountForHomeCMS");
     CountEN = await getAllData(URL_API + "Management/GetCountForHomeCMSEN");
-    $('#ApprovedTinTuc').text('Có ' + CountVI.CountNewsVnAll + n)
-    $('#ApprovedDuAn').text('Có ' + CountVI.CountDuAn + n)
-    $('#ApprovedHDTN').text('Có ' + CountVI.CountHDTNAll + n)
-    $('#ApprovedHTNC').text('Có ' + CountVI.CountHTNCAll + n)
-    $('#PendingTinTuc').text('Có ' + CountVI.CountNewsPending + n)
-    $('#PendingDuAn').text('Có ' + CountVI.CountDuAnPending + n)
-    $('#PendingHDTN').text('Có ' + CountVI.CountHDTNPending + n)
-    $('#PendingHTNC').text('Có ' + CountVI.CountHTNCPending + n)
-    $('#ApprovedTinTucEN').text('Có ' + CountEN.CountNewsEnAll + n)
-    $('#ApprovedDuAnEN').text('Có ' + CountEN.CountDuAn + n)
-    $('#ApprovedHDTNEN').text('Có ' + CountEN.CountHDTNAll + n)
-    $('#ApprovedHTNCEN').text('Có ' + CountEN.CountHTNCAll + n)
-    $('#PendingTinTucEN').text('Có ' + CountEN.CountNewsPending + n)
-    $('#PendingDuAnEN').text('Có ' + CountEN.CountDuAnPending + n)
-    $('#PendingHDTNEN').text('Có ' + CountEN.CountHDTNPending + n)
-    $('#PendingHTNCEN').text('Có ' + CountEN.CountHTNCPending + n)
-    $('#BaiDangVNPending').text('Có ' + Number(CountVI.CountDuAnPending + CountVI.CountHDTNPending + CountVI.CountHTNCPending) + ns)
-    $('#BaiDangENPending').text('Có ' + Number(CountEN.CountDuAnPending + CountEN.CountHDTNPending + CountEN.CountHTNCPending) + ns)
-    $('#TNVPending').text('Có ' + CountVI.CountVolunteerPending + ' tình nguyện viên chờ duyệt')
-    $('#TinTucVNPending').text('Có ' + CountVI.CountNewsPending + ns)
-    $('#TinTucENPending').text('Có ' + CountEN.CountNewsPending + ns)
-    $('#TaiKhoanPending').text('Có ' + CountVI.CountAccountPending + ' tài khoản đang chờ duyệt')
+    $('#ApprovedTinTuc').text(c + CountVI.CountNewsVnAll + n)
+    $('#ApprovedDuAn').text(c + CountVI.CountDuAn + n)
+    $('#ApprovedHDTN').text(c + CountVI.CountHDTNAll + n)
+    $('#ApprovedHTNC').text(c + CountVI.CountHTNCAll + n)
+    $('#PendingTinTuc').text(c + CountVI.CountNewsPending + n)
+    $('#PendingDuAn').text(c + CountVI.CountDuAnPending + n)
+    $('#PendingHDTN').text(c + CountVI.CountHDTNPending + n)
+    $('#PendingHTNC').text(c + CountVI.CountHTNCPending + n)
+    $('#ApprovedTinTucEN').text(c + CountEN.CountNewsEnAll + n)
+    $('#ApprovedDuAnEN').text(c + CountEN.CountDuAn + n)
+    $('#ApprovedHDTNEN').text(c + CountEN.CountHDTNAll + n)
+    $('#ApprovedHTNCEN').text(c + CountEN.CountHTNCAll + n)
+    $('#PendingTinTucEN').text(c + CountEN.CountNewsPending + n)
+    $('#PendingDuAnEN').text(c + CountEN.CountDuAnPending + n)
+    $('#PendingHDTNEN').text(c + CountEN.CountHDTNPending + n)
+    $('#PendingHTNCEN').text(c + CountEN.CountHTNCPending + n)
+    $('#BaiDangVNPending').text(c + Number(CountVI.CountDuAnPending + CountVI.CountHDTNPending + CountVI.CountHTNCPending) + ns)
+    $('#BaiDangENPending').text(c + Number(CountEN.CountDuAnPending + CountEN.CountHDTNPending + CountEN.CountHTNCPending) + ns)
+    $('#TNVPending').text(c + CountVI.CountVolunteerPending + ' tình nguyện viên chờ duyệt')
+    $('#TinTucVNPending').text(c + CountVI.CountNewsPending + ns)
+    $('#TinTucENPending').text(c + CountEN.CountNewsPending + ns)
+    $('#TaiKhoanPending').text(c + CountVI.CountAccountPending + ' tài khoản đang chờ duyệt')
 }
