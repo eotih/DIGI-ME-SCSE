03741df6ad54ce60cd5c2d531cdafd35e54d7c39
My Nguyen <myoutybay@gmail.com>
2021-11-20 16:37:05 +0700

    Crud Trang chinh

--- SCSE-DASHBOARD/pages/DAO/CrudTrangChinh.js
+++ SCSE-DASHBOARD/pages/DAO/CrudTrangChinh.js
@@ -1,33 +1,32 @@
-const URL_API = "https://api.scse-vietnam.org/API/Management/";
+const URL_API = "http://localhost:59360/API/";
 window.addEventListener('load', getNew);
-let NewsVN,NewsEN,PostVN,PostEN,Volunteers,PendingAccount = [];
+let NewsVN, NewsEN, PostVN, PostEN, Volunteers, PendingAccount = [];
 const getAllData = async (url) => {
     return (await fetch(url)).json();
 }
-async function getNew(){
-    NewsEN = await getAllData(URL_API + "ShowAllNewsEN");
-    NewsVN = await getAllData(URL_API + "ShowAllNewsVN");
-    PostVN = await getAllData(URL_API + "ShowAllPost");
-    PostEN = await getAllData(URL_API + "ShowAllPostEN");
-    Volunteers = await getAllData(URL_API + "ShowAllVolunteers");
-    PendingAccount = await getAllData("https://api.scse-vietnam.org/API/User/ShowAllAccount")
+async function getNew() {
+    CountVI = await getAllData(URL_API + "Management/GetCountForHomeCMS");
+    CountEN = await getAllData(URL_API + "Management/GetCountForHomeCMSEN");
+    $('#ApprovedTinTuc').text('Có ' + CountVI.CountNewsVnAll + ' bài')
+    $('#ApprovedDuAn').text('Có ' + CountVI.CountDuAn + ' bài')
+    $('#ApprovedHDTN').text('Có ' + CountVI.CountHDTNAll + ' bài')
+    $('#ApprovedHTNC').text('Có ' + CountVI.CountHTNCAll + ' bài')
+    $('#PendingTinTuc').text('Có ' + CountVI.CountNewsPending + ' bài')
+    $('#PendingDuAn').text('Có ' + CountVI.CountDuAnPending + ' bài')
+    $('#PendingHDTN').text('Có ' + CountVI.CountHDTNPending + ' bài')
+    $('#PendingHTNC').text('Có ' + CountVI.CountHTNCPending + ' bài')
+    $('#ApprovedTinTucEN').text('Có ' + CountEN.CountNewsEnAll + ' bài')
+    $('#ApprovedDuAnEN').text('Có ' + CountEN.CountDuAn + ' bài')
+    $('#ApprovedHDTNEN').text('Có ' + CountEN.CountHDTNAll + ' bài')
+    $('#ApprovedHTNCEN').text('Có ' + CountEN.CountHTNCAll + ' bài')
+    $('#PendingTinTucEN').text('Có ' + CountEN.CountNewsPending + ' bài')
+    $('#PendingDuAnEN').text('Có ' + CountEN.CountDuAnPending + ' bài')
+    $('#PendingHDTNEN').text('Có ' + CountEN.CountHDTNPending + ' bài')
+    $('#PendingHTNCEN').text('Có ' + CountEN.CountHTNCPending + ' bài')
+    $('#BaiDangVNPending').text('Có ' + Number(CountVI.CountDuAnPending + CountVI.CountHDTNPending + CountVI.CountHTNCPending) + ' bài viết hiện đang chờ duyệt')
+    $('#BaiDangENPending').text('Có ' + Number(CountEN.CountDuAnPending + CountEN.CountHDTNPending + CountEN.CountHTNCPending) + ' bài viết hiện đang chờ duyệt')
+    $('#TNVPending').text('Có ' + CountVI.CountVolunteerPending + ' tình nguyện viên chờ duyệt')
+    $('#TinTucVNPending').text('Có ' + CountVI.CountNewsPending + ' bài viết hiện đang chờ duyệt')
+    $('#TinTucENPending').text('Có ' + CountEN.CountNewsPending + ' bài viết hiện đang chờ duyệt')
+    $('#TaiKhoanPending').text('Có ' + CountVI.CountAccountPending + ' tài khoản đang chờ duyệt')
 }
-// ApprovedTinTuc
-// ApprovedDuAn
-// ApprovedHDTN
-// ApprovedHTNC
-
-// PendingTinTuc
-// PendingDuAn
-// PendingHDTN
-// PendingHTNC
-
-// ApprovedTinTucEN
-// ApprovedDuAnEN
-// ApprovedHDTNEN
-// ApprovedHTNCEN
-
-// PendingTinTucEN
-// PendingDuAnEN
-// PendingHDTNEN
-// PendingHTNCEN
